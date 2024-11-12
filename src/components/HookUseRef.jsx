aac8f6ffc6d1e37d70bab8dd350e92a9d07062a1
Daniel-derf <danielpardinho321@gmail.com>
2024-11-12 10:25:43 -0300

    useRef

---
+++ src/components/HookUseRef.jsx
@@ -0,0 +1,27 @@
+import { useRef, useState, useEffect } from 'react'
+
+const HookUseRef = () => {
+	// 1 - useRef
+	const numberRef = useRef(0);
+	const [counter, setCounter] = useState(0);
+	const [counterB, setCounterB] = useState(0);
+
+	useEffect(()=>{
+		numberRef.current = numberRef.current + 1
+	})
+
+	
+  return (
+    <div>
+        <h2>useRef</h2>
+				<p>O componente renderizou: {numberRef.current} vezes.</p>
+				<p>Counter 1: {counter}</p>
+				<p>Counter 2: {counterB}</p>
+				<button onClick={()=> setCounter(counter + 1)}>Contador A</button>
+				<button onClick={()=> setCounterB(counterB + 1)}>Contador B</button>
+        <hr />
+    </div>
+  )
+}
+
+export default HookUseRef
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import HookUseEffect from '../components/HookUseEffect'
 
 import { useContext } from 'react'
 import { SomeContext } from '../components/HookUseContext'
+import HookUseRef from '../components/HookUseRef'
 
 
 const Home = () => {
@@ -20,6 +21,7 @@ const Home = () => {
         <h2>useContext</h2>
         <p>Valor do context: {contextValue}</p>
         <hr />
+        <HookUseRef/>
     </div>
   )
 }
