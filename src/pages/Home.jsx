4846d1d4fb891008c6e67a6ea1ddd15b2d4d318a
Daniel-derf <danielpardinho321@gmail.com>
2024-11-12 10:54:17 -0300

    useCallback

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import HookUseEffect from '../components/HookUseEffect'
 import { useContext } from 'react'
 import { SomeContext } from '../components/HookUseContext'
 import HookUseRef from '../components/HookUseRef'
+import HookUseCallback from '../components/HookUseCallback'
 
 
 const Home = () => {
@@ -21,7 +22,8 @@ const Home = () => {
         <h2>useContext</h2>
         <p>Valor do context: {contextValue}</p>
         <hr />
-        <HookUseRef/>
+        <HookUseRef />
+        <HookUseCallback />
     </div>
   )
 }
